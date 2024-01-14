45e22c330da1e0ee11f15099a2efbbcdc3650f42
Zehra <Zehra@users.noreply.github.com>
2024-01-14 15:02:31 -0500

    Updated SpawnSave.cpp

--- SpawnSave.cpp
+++ SpawnSave.cpp
@@ -13,51 +13,75 @@ int isInRange(int player) {
 
 class SpawnSave : public bz_Plugin, bz_CustomSlashCommandHandler
 {
-    virtual const char* Name(){return "SpawnSave";}
-    virtual void Init ( const char* /*config*/ );
-    virtual void Event(bz_EventData *eventData );
-    virtual void Cleanup ( void );
-    virtual bool SlashCommand ( int playerID, bz_ApiString command, bz_ApiString message, bz_APIStringList* params );
-    public:
-    float playerPos[200][4] = { 0.0 }; // X, Y, Z and finally rotation as last.
-    int playerSavedSpawn[200] = { 0 }; // We check if they request a spawn position save.
-    // Easy way to reset spawns.
-    void resetSpawn(int playerID) {
-        playerPos[playerID][0] = 0.0; // x
-        playerPos[playerID][1] = 0.0; // Y
-        playerPos[playerID][2] = 0.0; // Z
-        playerPos[playerID][3] = 0.0; // Rotation
-        playerSavedSpawn[playerID] = 1;
-    }
-    
-};
-
-void SpawnSave::Init (const char*config) {
+  const char* Name(){return "SpawnSave";}
+  void Init (const char* commandLine) {
     bz_registerCustomSlashCommand ("save", this);
     Register(bz_ePlayerJoinEvent);
     Register(bz_eGetPlayerSpawnPosEvent);
     Register(bz_ePlayerPartEvent);
-}
-
-void SpawnSave::Cleanup (void) {
+  }
+void Event(bz_EventData *eventData );
+  void Cleanup (void) {
     bz_removeCustomSlashCommand ("save");
     Flush();
-}
+  }
+bool SlashCommand ( int playerID, bz_ApiString command, bz_ApiString message, bz_APIStringList* params );
+    public:
+    struct PlayerInfo {
+      float playerPosition[4];
+      int savedSpawn=0;
+      int resetAfter=0;
+    };
+    struct PlayerInfo PD[200];
+    // Easy way to reset spawns.
+    void resetSpawn(int playerID) {
+        for (int i=0;i<=3;i++) {
+          PD[playerID].playerPosition[i] = 0.0;
+        } // X // Y  // Z // Rotation
+        PD[playerID].savedSpawn = 0;
+        PD[playerID].resetAfter = 0;
+    }
+    
+};
 
 bool SpawnSave::SlashCommand ( int playerID, bz_ApiString command, bz_ApiString message, bz_APIStringList* params ) {
-    if ((!strcmp("save",command.c_str())) && (isInRange(playerID) == 1)) {
+  if ((!strcmp("save",command.c_str())) && (isInRange(playerID) == 1)) {
+    if (params->size()== 1) {
+      if ((!strcmp("pos",params->get(0).c_str())) || (!strcmp("fixed",params->get(0).c_str()))) {
+        // Could be rewritten, we quite often do this.
         bz_BasePlayerRecord *pr = bz_getPlayerByIndex(playerID);
         if (pr) {
-            playerPos[playerID][0] = pr->lastKnownState.pos[0]; // x
-            playerPos[playerID][1] = pr->lastKnownState.pos[1]; // Y
-            playerPos[playerID][2] = pr->lastKnownState.pos[2]; // Z
-            playerPos[playerID][3] = pr->lastKnownState.rotation; // Rotation
-            playerSavedSpawn[playerID] = 1;
-            bz_sendTextMessage(BZ_SERVER, playerID, "New spawn position saved.");
-            bz_freePlayerRecord(pr);
+          for (int i=0;i<=2;i++) { // 0=x, y=1, z=2.
+            PD[playerID].playerPosition[i] = pr->lastKnownState.pos[i];
+          }
+          PD[playerID].playerPosition[3] = pr->lastKnownState.rotation; // Rotation
+          PD[playerID].savedSpawn = 1;
+          bz_sendTextMessage(BZ_SERVER, playerID, "New spawn position saved.");  
         }
-        return true;
-    }
+        bz_freePlayerRecord(pr);
+        // In theory it should be a more efficient operation.
+        if (!strcmp("pos",params->get(0).c_str())) {
+          PD[playerID].resetAfter = 1;
+        } else {
+          PD[playerID].resetAfter = 0;
+        }
+      
+      }
+      if (!strcmp("clear",params->get(0).c_str())) {
+        resetSpawn(playerID);
+      }
+      
+      
+    } else {
+      bz_sendTextMessage(BZ_SERVER,playerID,"SpawnSave commands help:");
+      bz_sendTextMessage(BZ_SERVER,playerID,"/save         -  Current help page");
+      bz_sendTextMessage(BZ_SERVER,playerID,"/save pos     -  Saves position for only the next spawn.");
+		  bz_sendTextMessage(BZ_SERVER,playerID,"/save clear   -  Clears saved data.");
+		  bz_sendTextMessage(BZ_SERVER,playerID,"/save fixed   -  Saves position for all future spawns.");
+    }   
+  return true;
+  }
+  return false;
 }
 
 void SpawnSave::Event(bz_EventData *eventData ){
@@ -76,15 +100,17 @@ void SpawnSave::Event(bz_EventData *eventData ){
         int player = getSpawnData->playerID;
         //
         if (isInRange(player) == 1) {
-            if (playerSavedSpawn[player] == 1) {
+            if (PD[player].savedSpawn == 1) {
                 getSpawnData->handled = true;
-                getSpawnData->pos[0] = playerPos[player][0];
-                getSpawnData->pos[1] = playerPos[player][1];
-                getSpawnData->pos[2] = playerPos[player][2];
-                getSpawnData->rot = playerPos[player][3];
+                getSpawnData->pos[0] = PD[player].playerPosition[0];
+                getSpawnData->pos[1] = PD[player].playerPosition[1];
+                getSpawnData->pos[2] = PD[player].playerPosition[2];
+                getSpawnData->rot = PD[player].playerPosition[3];
+                if (PD[player].resetAfter == 1) {
+                  PD[player].savedSpawn = 0;
+                }
             }
         }
-
     }break;
     
     case bz_ePlayerPartEvent: {
@@ -101,3 +127,4 @@ void SpawnSave::Event(bz_EventData *eventData ){
 }
 
 BZ_PLUGIN(SpawnSave)
+
